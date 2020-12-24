0d6bf9261398db9bf41977e67488df04380bb4d4
meoflauta <meoflauta@gmail.com>
2020-12-25 00:14:40 +0800

    Display footer actions in post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native'
 import {width, height} from '../utilities/constants'
+import { Ionicons } from '@expo/vector-icons';
 
 const Post = ({post}) => {
 
-    console.log("hey post is: ", post)
+    function likeTapped(){
+        const [liked, setLiked] = useState(false);
+        let prevLike = liked
+        setLiked(!prevLike)
+    }
+    
+    
     return (
-        <View style={styles.container}>
-            <View style={styles.head}>
+        <View style={styles.container} >
+            <View style={styles.head} >
                 <Image style={styles.userPic} source={{uri: post.userPicture}} />
                 <Text>{post.username}</Text>
             </View>
             <Image style={styles.picture} source= {{uri: post.picture}} />
+            <View style={styles.actionsHolder} >
+                <View style={styles.actionsLeft}>
+
+                    <TouchableOpacity onPress={() => {
+                        likeTapped()
+                    }}> 
+                        <Ionicons 
+                            name="ios-heart" 
+                            size={32} 
+                            color="red"
+                            // color={liked ? "red" : "#d3d3d3" }
+                            style={{ marginRight: 10 }}/>
+                    
+                    </TouchableOpacity> 
+                    <Ionicons 
+                        name="ios-chatbubbles" 
+                        size={32} color="#d3d3d3" 
+                        style={{ marginRight: 10 }}/>
+                </View>
+                {/* <View style={styles.actionsRight}>
+                    <Ionicon name="ios-bookmark" size={32} color="#d3d3d3" />
+                </View> */}
+                
+                <View>
+                    <Text style={styles.likes}>{`${post.likes} likes`}</Text>
+                </View>
+
+                <View style={styles.containerText}>
+                    <Text style={styles.username}>{`${post.username}`}</Text>
+                    <Text style={styles.caption}>{`${post.caption}`}</Text>
+                </View>
+            </View>
         </View>
     )
 }
@@ -37,6 +76,38 @@ const styles = StyleSheet.create({
     },
     picture:{
         height: height / 2
+    },
+    actionsHolder:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingRight: 10,
+        paddingLeft: 10,
+        paddingTop: 5,
+        paddingBottom: 5
+    },
+    actionsLeft:{
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    likes: { 
+        fontSize: 18,
+        fontWeight: "600",
+        marginLeft: 10,
+    },
+    text: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingLeft: 10
+    },
+    caption: {
+        fontSize: 17,
+        fontWeight: "500",
+        marginLeft: 10
+    },
+    containerText:{
+        backgroundColor: "orange"
     }
 })
 
