9b610b29dc7d64e65cdde6231807097d6097eccb
meoflauta <meoflauta@gmail.com>
2020-12-24 08:18:14 +0800

    Provide initial base project that includes nested navigators

--- App.js
+++ App.js
@@ -1,13 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Feed from './src/screens/Feed'
+import Explore from './src/screens/Explore'
+import Notifications from './src/screens/Notifications'
+import Profile from './src/screens/Profile'
+import Dm from './src/screens/Dm'
+import Story from './src/screens/Story'
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const BottomTab = createMaterialBottomTabNavigator();
+const TopTab = createMaterialTopTabNavigator();
+const FeedStack = createStackNavigator();
+const DmStack = createStackNavigator();
+
+const FeedTabs = () => {
+  return (
+    <TopTab.Navigator>
+      <TopTab.Screen
+        name="FeedStack"
+        children={() => {
+          <FeedStack.Navigator>
+            <FeedStack.Screen
+              name="Feed"
+              component={Feed}
+            />
+            <FeedStack.Screen
+              name="Story"
+              component={Story}
+            />
+          </FeedStack.Navigator>
+        }}
+      />
+      <TopTab.Screen
+        name="DmStack"
+        children={() => {
+          <DmStack.Navigator>
+            <DmStack.Screen name="Dm" component={Dm} />
+          </DmStack.Navigator>
+        }}
+      />
+    </TopTab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        <BottomTab.Screen 
+          name="Feed" 
+          children={() => FeedTabs()} 
+        />
+        <BottomTab.Screen name="Explore" component={Explore} />
+        <BottomTab.Screen name="Notifications" component={Notifications} />
+        <BottomTab.Screen name="Profile" component={Profile} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/Dm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Dm = () => {
+    return (
+        <View>
+            <Text>Dm</Text>
+        </View>
+    )
+}
+
+export default Dm
